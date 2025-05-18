ce7f545ed41e6fc348c06985175b5cc909c09828
raphael330 <108478012+raphael330@users.noreply.github.com>
2025-05-17 23:41:27 -0300

    confirmar

---
+++ script.js
@@ -0,0 +1,3 @@
+<button onclick="document.getElementById('talvez').innerHTML = 'A ideia é nunca perder a motivação para subir cada degrau rumo ao sucesso, por mais difícil que seja. E também não esquecer de agradecer pelas lições aprendidas em cada etapa da jornada.'">
+        Clique para ver uma mensagem
+    </button>
